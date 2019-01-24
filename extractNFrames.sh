a4b19b7638236105f43a8b1376dc56b4096ff992
hemant <hemantj@uw.edu>
2019-01-23 16:56:48 -0800

    edit to deal with multiple streams in videos

--- extractNFrames.sh
+++ extractNFrames.sh
@@ -1,5 +1,7 @@
-#/bin/sh
-total_frames=`ffprobe -loglevel panic -show_streams $1 | grep "^nb_frames" | cut -d '=' -f 2`
+#!/bin/sh
+rm -rf data/tmp/*.jpg
+total_frames=`ffprobe -loglevel panic -show_streams $1 -select_streams v | grep "^nb_frames" | cut -d '=' -f 2`
+# echo $total_frames
 numframes=$2
 rate=`echo "scale=0; $total_frames/$numframes" | bc`
 # echo $rate
