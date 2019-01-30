a270288fe76a01faa2889ef5bcd4b1573006faa3
hemant <hemantj@uw.edu>
2019-01-30 12:52:18 -0800

    Extract N frames from UCF101 videos

---
+++ extractNFrames_UCF101.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir -p data/tmp/$3
+# rm -rf /tmp/*.jpg
+total_frames=`ffprobe -loglevel panic -show_streams $1 -select_streams v | grep "^nb_frames" | cut -d '=' -f 2`
+# echo $total_frames
+numframes=$2
+rate=`echo "scale=0; $total_frames/$numframes" | bc`
+# echo $rate
+ffmpeg -loglevel panic -i $1 -f image2 -vf scale="426x240","select='not(mod(n,$rate))'" -vframes $numframes -vsync vfr data/tmp/$3/output_%03d.jpg
