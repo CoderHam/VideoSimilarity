8570fa676dbd97ce59613a578b4abc75231aaa23
heybaebae <ryanbae89@gmail.com>
2019-02-17 12:30:32 -0800

    added CPU option to kNN and 3d-cnn submodule

--- knn_cnn_features.py
+++ knn_cnn_features.py
@@ -4,20 +4,21 @@ import sys
 
 import knn_gpu
 
-def run_knn_features(feature_vectors,test_vectors=None,k=5,flat=True,verbose=False,dist=False):
+def run_knn_features(feature_vectors, test_vectors=None, k=5, flat=True,
+                     verbose=False, dist=False, gpu=False):
     nb, d = feature_vectors.shape
     if type(test_vectors) is not np.ndarray:
         if flat:
-            D, I = knn_gpu.knn_flat(feature_vectors, feature_vectors, d, k, verbose)
+            D, I = knn_gpu.knn_flat(feature_vectors, feature_vectors, d, k, verbose, gpu)
         else:
-            D, I = knn_gpu.knn_ivf(feature_vectors, feature_vectors, d, k, verbose)
+            D, I = knn_gpu.knn_ivf(feature_vectors, feature_vectors, d, k, verbose, gpu)
     else:
         if flat:
-            D, I = knn_gpu.knn_flat(feature_vectors, test_vectors, d, k, verbose)
+            D, I = knn_gpu.knn_flat(feature_vectors, test_vectors, d, k, verbose, gpu)
         else:
-            D, I = knn_gpu.knn_ivf(feature_vectors, test_vectors, d, k, verbose)
+            D, I = knn_gpu.knn_ivf(feature_vectors, test_vectors, d, k, verbose, gpu)
 
     if dist:
-        return D, I 
+        return D, I
     else:
         return I
--- knn_gpu.py
+++ knn_gpu.py
@@ -2,46 +2,48 @@ import numpy as np
 import faiss
 
 ## Using a flat index
-def knn_flat(vector_x, queries_x, d, k, verbose):
-    res = faiss.StandardGpuResources()  # use a single GPU
+def knn_flat(vector_x, queries_x, d, k, verbose, gpu=True):
     index_flat = faiss.IndexFlatL2(d)  # build a flat (CPU) index
+    if gpu:
+        res = faiss.StandardGpuResources()  # use a single GPU
+        index_flat = faiss.index_cpu_to_gpu(res, 0, index_flat)
+    index_flat.add(vector_x)
 
-    # make it a flat GPU index
-    gpu_index_flat = faiss.index_cpu_to_gpu(res, 0, index_flat)
-
-    gpu_index_flat.add(vector_x)         # add vectors to the index
     if verbose:
-        print("Running with Flat index for",gpu_index_flat.ntotal,"records of with dimensionality",d)
-
+        print("Running with Flat index for", index_flat.ntotal,
+             "records of with dimensionality", d)
     # we want to see the k nearest neighbors
-    D, I = gpu_index_flat.search(queries_x, k)  # actual search
+    D, I = index_flat.search(queries_x, k) # actual search
     return D, I
 
 # Using an IVF index
-def knn_ivf(vector_x, queries_x, d, k, verbose):
+def knn_ivf(vector_x, queries_x, d, k, verbose, gpu=True):
     nlist = 100
     quantizer = faiss.IndexFlatL2(d)  # the other index
     index_ivf = faiss.IndexIVFFlat(quantizer, d, nlist, faiss.METRIC_L2)
     # here we specify METRIC_L2, by default it performs inner-product search
-
     # make it an IVF GPU index
-    gpu_index_ivf = faiss.index_cpu_to_gpu(res, 0, index_ivf)
+    if gpu:
+        res = faiss.StandardGpuResources()  # use a single GPU
+        index_ivf = faiss.index_cpu_to_gpu(res, 0, index_ivf)
 
-    assert not gpu_index_ivf.is_trained
-    gpu_index_ivf.train(vector_x)        # add vectors to the index
-    assert gpu_index_ivf.is_trained
+    assert not index_ivf.is_trained
+    index_ivf.train(vector_x)        # add vectors to the index
+    assert index_ivf.is_trained
+    index_ivf.add(vector_x)          # add vectors to the index
 
-    gpu_index_ivf.add(vector_x)          # add vectors to the index
-    print("Running with IVF index for",gpu_index_ivf.ntotal,"records of with dimensionality",d)
+    if verbose:
+        print("Running with IVF index for", gpu_index_ivf.ntotal,
+              "records of with dimensionality", d)
 
-    D, I = gpu_index_ivf.search(queries_x, k)  # actual search
+    D, I = index_ivf.search(queries_x, k)  # actual search
     return D, I
 
-def run(k,flat=True,verbose=False):
+def run(k, flat=True, verbose=False):
     # create fake data
     d = 200                           # dimensionality
     nb = 1000000                      # number of records
-    nq = 10**4                       # number of queries
+    nq = 10                       # number of queries
     np.random.seed(1234)
     vector_x = np.random.random((nb, d)).astype('float32')
     vector_x[:, 0] += np.arange(nb) / 1000.
@@ -50,10 +52,12 @@ def run(k,flat=True,verbose=False):
 
     # perform knn search
     if flat:
-        D, I = knn_flat(vector_x, queries_x, d, k, verbose=verbose)
+        D, I = knn_flat(vector_x, queries_x, d, k,
+                        verbose=verbose, gpu=False)
     else:
-        D, I = knn_ivf(vector_x, queries_x, d, k, verbose=verbose)
+        D, I = knn_ivf(vector_x, queries_x, d, k,
+                        verbose=verbose, gpu=False)
     # print(I[:5])                   # neighbors of the 5 first queries
     # print(list(zip(D[:5],I[:5])))
 
-# run(10)
+run(10)
