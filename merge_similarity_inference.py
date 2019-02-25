80521c5b9fdcea5c849f14002b34b45e20bda8f9
hemant <hemantj@uw.edu>
2019-02-24 21:33:12 -0800

    tests and adding cnn 3d KNN search

--- merge_similarity_inference.py
+++ merge_similarity_inference.py
@@ -16,16 +16,17 @@ def merge_similarity_ucf_video(vid_path, k=10, verbose=True):
     color_dist, color_indices = color_similarity_inference.similar_color_ucf_video(vid_path, k=k, dist=True)
     feature_dist, feature_indices = feature_similarity_inference.similar_feature_ucf_video(vid_path, k=k, dist=True)
     sound_dist, sound_indices = sound_similarity_inference.similar_sound_ucf_video(vid_path, k=k, dist=True)
-    # cnn3d_dist, cnn3d_indices = cnn3d_similarity_inference.similar_cnn3d_ucf_video(vid_path, k=k, dist=True)
-    sorted_listed = [x for _,x in sorted(zip(color_dist+feature_dist+sound_dist, color_indices+feature_indices+sound_indices))]
-    uniq_sorted_listed, uniq_sorted_dist = get_ordered_unique(sorted_listed, sorted(color_dist+feature_dist+sound_dist))
+    cnn3d_dist, cnn3d_indices = cnn3d_similarity_inference.similar_cnn3d_ucf_video(vid_path, k=k, dist=True)
+
+    sorted_listed = [x for _,x in sorted(zip(color_dist+feature_dist+sound_dist+cnn3d_dist, color_indices+feature_indices+sound_indices+cnn3d_indices))]
+    uniq_sorted_listed, uniq_sorted_dist = get_ordered_unique(sorted_listed, sorted(color_dist+feature_dist+sound_dist+cnn3d_dist))
     if verbose:
         print(uniq_sorted_listed[:k], uniq_sorted_dist[:k])
     return uniq_sorted_listed[:k]
 
 # import time
 # start = time.time()
-# for i in range(10):
-#     merge_similarity_ucf_video('data/UCF101/v_ApplyEyeMakeup_g01_c01.avi', verbose=True)
+# # # for i in range(10):
+# merge_similarity_ucf_video('data/UCF101/v_ApplyEyeMakeup_g01_c01.avi', verbose=True)
 # print((time.time()-start)/10)
 #  3.8777024269104006 seconds
