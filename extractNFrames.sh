650155235d29f92f01ca93b03d1f12e05a1c8b3b
hemant <hemantj@uw.edu>
2019-03-04 16:58:19 -0800

    changes to correct error with webm files

--- extractNFrames.sh
+++ extractNFrames.sh
@@ -3,6 +3,13 @@ rm -rf data/tmp/*.jpg
 total_frames=`ffprobe -loglevel panic -show_streams $1 -select_streams v | grep "^nb_frames" | cut -d '=' -f 2`
 # echo $total_frames
 numframes=$2
-rate=`echo "scale=0; $total_frames/$numframes" | bc`
-# echo $rate
+if [ $total_frame=="N/A" ]
+then
+  frame_rate=`ffprobe -loglevel panic -show_streams $1 -select_streams v | grep "^r_frame_rate" | cut -d '=' -f 2`
+  rate=`echo ${frame_rate%/*} | bc`
+  # echo $rate
+else
+  rate=`echo "scale=0; $total_frames/$numframes" | bc`
+  # echo $rate
+fi
 ffmpeg -loglevel panic -i $1 -f image2 -vf scale="426x240","select='not(mod(n,$rate))'" -vframes $numframes -vsync vfr data/tmp/output_%03d.jpg
--- knn_cnn_features.py
+++ knn_cnn_features.py
@@ -1,7 +1,6 @@
 import numpy as np
 import faiss
 import sys
-
 import knn_gpu
 
 def run_knn_features(feature_vectors, test_vectors=None, k=5, flat=False,
@@ -17,7 +16,6 @@ def run_knn_features(feature_vectors, test_vectors=None, k=5, flat=False,
             D, I = knn_gpu.knn_flat(feature_vectors, test_vectors, d, k, verbose, gpu)
         else:
             D, I = knn_gpu.knn_ivf(feature_vectors, test_vectors, d, k, verbose, gpu)
-
     if dist:
         return D, I
     else:
